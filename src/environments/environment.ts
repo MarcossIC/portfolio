7b5be31cb40a8fcc77c0dc0fc1e679050976107a
Marcos Lopez <marcoslopezdev18@gmail.com>
2023-10-06 23:29:07 -0300

    tray fix deploy routing error

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000/' // URL de la API para desarrollo
+    apiUrl: 'http://localhost:4200/' // URL de la API para desarrollo
   };
