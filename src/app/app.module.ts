cbdcf00b5b18a2804af167f47bd7dcf5dabaeaed
Marcos Lopez <marcoslopezdev18@gmail.com>
2023-10-29 16:53:11 -0300

    fix dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { AppRoutes } from './app.routing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
