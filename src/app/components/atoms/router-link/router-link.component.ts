7a2f2d7867da3a2746ab99b7063531fe72c863cf
Marcos Lopez <marcoslopezdev18@gmail.com>
2024-06-17 11:27:26 -0300

    refactor: Change navigate fc for routerlink and fragment

--- src/app/components/atoms/router-link/router-link.component.ts
+++ src/app/components/atoms/router-link/router-link.component.ts
@@ -12,8 +12,8 @@ import { Router, RouterModule } from '@angular/router';
   selector: 'router-link',
   template: ` <a
     [ngClass]="classNames"
-    [routerLink]="['']"
-    (click)="navigate(path, fragment)"
+    [routerLink]="[path]"
+    [fragment]="fragment"
     role="link"
     class="relative group outline-none focus:outline-none focus-within:outline-none"
   >
@@ -27,16 +27,4 @@ export class RouterLinkComponent {
   @Input() public path = '';
   @Input() public fragment = '';
   @Input() public classNames = '';
-
-  private readonly router: Router = inject(Router);
-  private readonly viewportScroller: ViewportScroller =
-    inject(ViewportScroller);
-
-  protected navigate(path: string, fragment: string) {
-    const usePath = path === '/' || path === '' ? [] : [path];
-
-    this.router.navigate(usePath, { replaceUrl: true }).then(() => {
-      this.viewportScroller.scrollToAnchor(fragment);
-    });
-  }
 }
